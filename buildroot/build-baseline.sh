d720131f17326cbc67be33a017d1c84e62e74c3f
rainbow-pi <rainbow-pi@outlook.com>
2018-11-25 10:14:19 +0800

    Fix a bug : wrong buildroot config file name.

--- buildroot/build-baseline.sh
+++ buildroot/build-baseline.sh
@@ -5,7 +5,7 @@ OUTDIR=./output-rainbow-pi-baseline/
 mkdir -p ${OUTDIR}
 
 if [ ! -f ${OUTDIR}.config ]; then
-    cp ${BUILDROOT_DIR}../configs/buildroot.config.qt ${OUTDIR}.config
+    cp ${BUILDROOT_DIR}../configs/buildroot.config.baseline ${OUTDIR}.config
     make O=${OUTDIR} oldconfig
 fi
 
